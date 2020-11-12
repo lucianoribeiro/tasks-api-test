366637e503aaad76ebe54b75e98e4d81d0e8a22c
Luciano Pinheiro Ribeiro <lucianopinheiroribeiro@gmail.com>
2020-11-12 07:22:32 -0300

    alteracao da data no metodo inserir tarefa com sucesso

--- src/test/java/br/lucianopinheiro/tasks/apitest/APITest.java
+++ src/test/java/br/lucianopinheiro/tasks/apitest/APITest.java
@@ -34,7 +34,7 @@ public class APITest {
 	public void deveAdicionarTarefacomSucesso() {
 		
 		RestAssured.given()
-		.body("{ \"task\": \"Testes via API\",	\"dueDate\": \"2020-12-29\"}")
+		.body("{ \"task\": \"Testes via API\",	\"dueDate\": \"2020-12-30\"}")
 		.contentType(io.restassured.http.ContentType.JSON)
 		
 		.when()
